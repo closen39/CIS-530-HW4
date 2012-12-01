4e3409060c388a1a2bd67329641e132591919bc7
Nate Close <closen@seas.upenn.edu>
2012-12-01 13:14:49 -0500

    added main method

--- reformat.py
+++ reformat.py
@@ -4,3 +4,9 @@ def reformat():
     for line in f:
         out.write('/home1/c/cis530/hw4/data/' + line.rstrip() + "\n")
     out.close()
+
+def main():
+    reformat()
+
+if __name__ == '__main__':
+    main()
