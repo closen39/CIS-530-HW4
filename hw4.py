ccca4bb205d147b93295a45bea213ed0f606d342
Jason Mow <jason.mow@gmail.com>
2012-12-01 15:08:22 -0500

    top noun verbs done

--- hw4.py
+++ hw4.py
@@ -3,6 +3,7 @@
 
 from nltk.corpus import PlaintextCorpusReader
 from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
 from BeautifulSoup import BeautifulSoup as Soup
 
 def get_all_files(directory):
@@ -40,12 +41,34 @@ def get_nounverb_lemma_dict(tok_sents, tagmap):
                 retDict[tup[0]] = tup[1]
     return retDict
 
+def get_func_words(filename):
+    retList = list()
+    f = open(filename)
+    for line in f:
+        retList.append(line.rstrip())
+    return retList
+
+# Assuming that we check the lemma for existence in funcwords list
+def get_top_nouns_verbs(tok_sents, tagmap, n):
+    # get_func_words('/home1/c/cis530/hw4/funcwords.txt')
+    funcwords = get_func_words('funcwords.txt')
+    fdNoun = FreqDist()
+    fdVerb = FreqDist()
+    for sent in tok_sents:
+        for tup in sent:
+            if tagmap[tup[2]] == "VERB" and tup[1] not in funcwords:
+                fdVerb.inc(tup[1]) 
+            elif tagmap[tup[2]] == "NOUN" and tup[1] not in funcwords:
+                fdNoun.inc(tup[1])
+    return (fdNoun.keys()[:n], fdVerb.keys()[:n])
+
 def main():
     tagmap = get_tag_mapping('en-ptb-modified.map')
     files = get_all_files('xmlDir')
     toks = get_all_sent_tok(files)
-    nvDict = get_nounverb_lemma_dict(toks, tagmap)
-    print nvDict
+    # nvDict = get_nounverb_lemma_dict(toks, tagmap)
+    topnv = get_top_nouns_verbs(toks, tagmap, 10)
+    print topnv
 
 if  __name__ =='__main__':
     main()
