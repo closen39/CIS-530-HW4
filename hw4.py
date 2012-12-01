7752dc0756a3eb564d384affc9835eb723112983
Jason Mow <jason.mow@gmail.com>
2012-12-01 13:05:11 -0500

    get_tag_mapping

--- hw4.py
+++ hw4.py
@@ -4,3 +4,11 @@
 from nltk.corpus import PlaintextCorpusReader
 from nltk.tokenize import word_tokenize
 
+def get_tag_mapping(map_file):
+    tags = dict()
+    f = open(map_file)
+    for line in f:
+        data = line.split("\t")
+        tags[data[0]] = data[1]
+    return tags
+
