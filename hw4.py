7a92ad2c01f03a9d9e92be78ce3a814a1ff70d01
Jason Mow <jason.mow@gmail.com>
2012-12-01 13:32:11 -0500

    get_all_sent_tok try 1

--- hw4.py
+++ hw4.py
@@ -3,6 +3,7 @@
 
 from nltk.corpus import PlaintextCorpusReader
 from nltk.tokenize import word_tokenize
+from BeautifulSoup import BeautifulSoup as Soup
 
 def get_tag_mapping(map_file):
     tags = dict()
@@ -12,3 +13,19 @@ def get_tag_mapping(map_file):
         tags[data[0]] = data[1]
     return tags
 
+# Assuming that xml_files is a list of filenames to xml files, not the actual xml
+def get_all_sent_tok(xml_files):
+    retList = list()
+    for file in xml_files:
+        text = open(file).read()
+        doc = Soup(text)
+        sents = [x.string for x in doc.findAll("sentence")]
+        for s in sents:
+            sentList = list()
+            sentence = Soup(s)
+            toks = [x.string for x in sentence.findAll("token")
+            for tok in toks:
+                token = Soup(tok)
+                sentList.append((token.find("word").string.lower(), token.find("lemma").string.lower(), token.find("pos").string.lower()))
+            retList.append(sentList)
+    return retList
